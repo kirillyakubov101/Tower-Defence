8186aeb791b7cdcec8b5cffc4d53975ccffd0aec
kirillyakubov <kirillyakubov101@gmail.com>
2020-10-08 11:01:27 -0400

    Commit - start build UI

---
+++ Assets/Scripts/UI/BuildUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildUI : MonoBehaviour
+{
+	[SerializeField] LayerMask towerMask;
+
+	private void Update()
+	{
+		if(Input.GetMouseButtonDown(1))
+		{
+			RaycastHit hit;
+			bool hasHit = Physics.Raycast(GetMouseRay(), out hit, towerMask);
+			if (hasHit)
+			{
+	
+			}
+			
+		}
+	}
+
+	private Ray GetMouseRay()
+	{
+		return Camera.main.ScreenPointToRay(MousePos());
+	}
+
+	private Vector3 MousePos()
+	{
+		return Input.mousePosition;
+	}
+
+
+}
