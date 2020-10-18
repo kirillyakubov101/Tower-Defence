65abf59cbad95313655bf146baa51f0244a7bd4f
kirillyakubov <kirillyakubov101@gmail.com>
2020-10-18 01:00:58 -0400

    no message

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	[SerializeField] float arrowSpeed;
+	[SerializeField] Health target; //remove the serialize
+
+
+	float damage;
+	bool isHit = false;
+
+
+	// Update is called once per frame
+	void Update()
+    {
+		if(target == null || isHit) { Destroy(gameObject); return; }
+		transform.LookAt(target.transform.position);
+		transform.Translate(Vector3.forward * arrowSpeed * Time.deltaTime);
+    }
+
+	public void AssignTarget(Health newTarget, float damage)
+	{
+		this.damage = damage;
+		target = newTarget;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Ground")
+		{
+			isHit = true;
+			Destroy(gameObject, 5f);
+		}
+
+		Health enemy = other.gameObject.GetComponent<Health>();
+
+		if (enemy != null)
+		{
+			enemy.takeDamage(damage);
+			Destroy(gameObject);
+		}
+		
+	}
+}
--- Assets/Scripts/UI/BuildUI.cs
+++ Assets/Scripts/UI/BuildUI.cs
@@ -3,59 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace TowerDefence.UI
+public class BuildUi : MonoBehaviour
 {
-	public class BuildUi : MonoBehaviour
+	[SerializeField] Image buildImage;
+	
+
+	
+	private void Awake()
 	{
-		[SerializeField] Image buildImage;
-		[SerializeField] GameObject ghostTower;
-		[SerializeField] GameObject flag;
+		buildImage.enabled = false;
+	}
 
-		private void Awake()
+
+	public void ActivateOnlyOneUiElement()
+	{
+		BuildUi[] buidUiObjects = FindObjectsOfType<BuildUi>();
+		foreach(BuildUi uiElement in buidUiObjects)
 		{
-			buildImage.enabled = false;
-			ghostTower.SetActive(false);
-		}
-
-
-		public void ActivateOnlyOneUiElement()
-		{
-			BuildUi[] buidUiObjects = FindObjectsOfType<BuildUi>();
-			foreach (BuildUi uiElement in buidUiObjects)
-			{
-				if (this != uiElement)
-				{
-					uiElement.GetBuildUiImage().enabled = false;
-					uiElement.ghostTower.SetActive(false);
-					uiElement.flag.SetActive(true);
-
-
-				}
-				else
-				{
-					uiElement.GetBuildUiImage().enabled = true;
-					uiElement.ghostTower.SetActive(true);
-					uiElement.flag.SetActive(false);
-				}
-			}
-		}
-
-		private Image GetBuildUiImage()
-		{
-			return buildImage;
-		}
-
-		public static void DisableAll()
-		{
-			var allBuildUi = FindObjectsOfType<BuildUi>();
-			foreach (BuildUi uiElement in allBuildUi)
+			if(this != uiElement)
 			{
 				uiElement.GetBuildUiImage().enabled = false;
-				uiElement.ghostTower.SetActive(false);
-				uiElement.flag.SetActive(true);
+			}
+			else
+			{
+				uiElement.GetBuildUiImage().enabled = true;
 			}
 		}
-
 	}
-}
 
+	private Image GetBuildUiImage()
+	{
+		return buildImage;
+	}
+
+	
+}
